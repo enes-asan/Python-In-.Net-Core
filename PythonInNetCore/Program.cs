860dea03e3f238ed2b057b90e7f7946af915d336
Enes ASAN <82983724+easan60@users.noreply.github.com>
2021-09-26 16:28:42 +0300

    datas

---
+++ PythonInNetCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using IronPython.Hosting;
+using System;
+
+namespace PythonInNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	//install IronPython packes from nuget
+            var source = @"C:\Users\easan\Desktop\PythonInNetCore\PythonInNetCore\PythonInNetCore\PythonFiles\Calculate.py";
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            var operation = engine.Operations;
+            engine.ExecuteFile(source, scope);
+            var calculate = scope.GetVariable("Calculate");
+            dynamic ins = operation.CreateInstance(calculate);
+
+            Console.WriteLine($"Sum={ins.sum(25,12)}");
+            Console.WriteLine($"Multiply={ins.multiply(25, 12)}");
+            Console.WriteLine($"Difference={ins.difference(25, 12)}");
+            Console.WriteLine($"Divide={ins.divide(25, 12)}");
+        }
+    }
+}
---
+++ PythonInNetCore/PythonFiles/Calculate.py
@@ -0,0 +1,11 @@
+class Calculate():
+    def __init__(self):
+        pass;
+    def sum(self,x,y):
+        return (x+y)
+    def multiply(self,x,y):
+        return (x*y)
+    def difference(self,x,y):
+        return (x-y)
+    def divide(self,x,y):
+        return (x/y)
